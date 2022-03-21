c08eb092e9fc5b49d2f74c8b5e38892a83666c73
Diego Galeano <diegogaleano@Diegos-iMac.local>
2022-03-21 11:04:48 -0300

    Trabajando los horarios por cada clase. solo terminé clarinete

---
+++ main.js
@@ -0,0 +1,14 @@
+//ejercicio//
+
+/*let nombre = prompt("ingrese su nombre");
+let apellido = prompt("cual es tu apellido");
+alert("Hola " + nombre + " " + apellido + " Bienvenido a Javascript");
+console.log(typeof nombre);
+*/
+
+//ejercicio2
+/*
+let num1 = prompt("ingrese un numero");
+let num2 = prompt("ingrese otro numero");
+alert("la sumas es igual a " + (Number(num1) + Number(num2)));
+*/
